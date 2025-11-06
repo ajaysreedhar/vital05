2cf755c7ddc4f382daee677666e099f3259d2db9
ajaysreedhar <sree.ajay468@gmail.com>
2025-11-07 00:22:25 +0530

    chore: hang kmain with WFE instruction

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,5 +1,13 @@
+#include "hardware/uart.h"
+
 void kmain() {
+    uart0_init();
+    uart0_write("Hello from Raspberry Pi 4!\n");
+    uart0_write(
+        "If you are reading this message, UART0 is initialized properly!\n");
+    uart0_write("This time, the communication is over PL011 UART interface.\n");
+
     while (1) {
-        // Hang forever...
+        asm volatile("wfe");
     }
 }
