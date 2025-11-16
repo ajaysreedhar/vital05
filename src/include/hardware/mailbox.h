f6c23922355d21c132e93acabf1e5e537e7ae463
ajaysreedhar <sree.ajay468@gmail.com>
2025-11-16 22:21:40 +0530

    chore: declare extern mailbox buffer

--- src/include/hardware/mailbox.h
+++ src/include/hardware/mailbox.h
@@ -1,4 +1,10 @@
 #ifndef HARDWARE_MAILBOX_H
 #define HARDWARE_MAILBOX_H
 
+#define MBOX_BUFFER_SIZE 36
+
+enum { MBOX_REQUEST_CODE = 0x0 };
+
+extern volatile unsigned int mailbox_buffer[MBOX_BUFFER_SIZE];
+
 #endif // #ifndef HARDWARE_MAILBOX_H
